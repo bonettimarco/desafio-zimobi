28730ba36754394beb104c8ceca2486ff1466b63
Marco Bonetti <marco_bonetti@uol.com.br>
2019-07-28 18:01:01 -0300

    consertando db

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
   end
 
   def anuncio usuario
-    "#{link_to "Novo", new_sale_path(usuario_id: usuario.id)}".html_safe if Sale.por_usuario(usuario).nil?
+    "#{link_to "Novo", new_sale_path(usuario_id: usuario.id)}".html_safe if usuario.sale.nil?
     link_to "Ver anúncio", sale_path(usuario.sale)
   end
 end
